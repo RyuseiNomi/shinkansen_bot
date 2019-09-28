290b874f5be5a215b04f758bf24d75ae2677b30b
RyuseiNomi <kanontonto1010@gmail.com>
2019-09-28 14:42:48 +0900

    set the seed value to rand struct

--- text/text.go
+++ text/text.go
@@ -1,12 +1,16 @@
 package text
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func GetTweetText() string {
 	stations := [13]string{
 		"新高岡", "黒部宇奈月温泉", "糸魚川", "上越妙高", "飯山", "上田", "佐久平", "軽井沢", "安中榛名", "高崎", "本庄早稲田", "熊谷", "上野",
 	}
 
-	text := "ただ今 " + stations[rand.Intn(12)] + " 付近を通過中"
+	rand.Seed(time.Now().UnixNano())
+	text := "ただ今 " + stations[rand.Intn(len(stations))] + " 付近を通過中"
 	return text
 }
